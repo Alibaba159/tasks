c20e034124dda73a83cb8ca4e5ba6c998a4efe60
Murenko Anastasiya <gloria19.09.1992@gmail.com>
2016-09-28 19:50:36 +0300

    Task1_edit_reverse

--- src/com/company/Task1/ArgsReverseOut.java
+++ src/com/company/Task1/ArgsReverseOut.java
@@ -7,6 +7,6 @@ public class ArgsReverseOut {
     public static void out(String[] args) {
         System.out.println("Args in reverse order");
         for (int i = args.length - 1; i >= 0; i--)
-            System.out.println("args[" + 2 + "]=" + args[i]);
+            System.out.print(args[i]+"; ");
     }
 }
